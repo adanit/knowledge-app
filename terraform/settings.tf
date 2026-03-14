f07555a206e1d770f9b55b7b2dd171684419ad93
Adan Martinez <adan.it@hotmail.com>
2026-03-14 18:47:36 -0300

    feat: Adiciona estrutura inicial de CI/CD

--- terraform/settings.tf
+++ terraform/settings.tf
@@ -3,8 +3,8 @@
 # ou usando: terraform init -backend-config="access_key=<sua_chave>"
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-labinfo-prod-brs-001"
-    storage_account_name = "strtfstatelabinfo001"
+    resource_group_name  = "rg-labinfo-prod-eus2-001"
+    storage_account_name = "strterraformbackend001"
     container_name       = "backend-terraform"
     key                  = "terraform-cilab.tfstate"
     # access_key é lido da variável de ambiente ARM_ACCESS_KEY
