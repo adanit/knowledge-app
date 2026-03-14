7b13e27f8b58c1c10514956ffa9e8b0a9a294e13
Adan Martinez <adan.it@hotmail.com>
2026-03-14 20:25:00 -0300

    feat: Adiciona estrutura inicial de CI/CD

--- terraform/containerregistry/main.tf
+++ terraform/containerregistry/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = var.rg_name
   location            = var.cr_location
   sku                 = var.cr_sku
+  admin_enabled       = true
   georeplications {
     location                = "East US"
   }
